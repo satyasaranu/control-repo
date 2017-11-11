20c154736d967868edd539c7e3f26ed16b83290b
satyasaranu <saranusys@gmail.com>
2017-11-11 11:59:25 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -47,7 +47,7 @@ class { '::tomcat': }
 class { '::java': }
 
 tomcat::install { '/opt/tomcat9':
-  source_url => 'https://www.apache.org/dist/tomcat/tomcat-9/v9.0.x/bin/apache-tomcat-9.0.x.tar.gz'
+  source_url => 'https://www.apache.org/dist/tomcat/tomcat-9/v9.0.1/bin/apache-tomcat-9.0.1.tar.gz'
 }
 tomcat::instance { 'tomcat9-first':
   catalina_home => '/opt/tomcat9',
